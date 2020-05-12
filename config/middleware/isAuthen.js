15edaf78dd47664a629dc456048e5dcb814d6178
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-05-11 19:27:29 -0500

    Working on login functionality

---
+++ config/middleware/isAuthen.js
@@ -0,0 +1,10 @@
+module.exports = function(req, res, next) {
+    // If logged in, continue 
+    if (req.user) {
+      return next();
+    }
+  
+    // else redirect to home
+    return res.redirect("/");
+  };
+  
