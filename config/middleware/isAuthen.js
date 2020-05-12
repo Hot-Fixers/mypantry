ebdf71d8d2896ea13861fba7dbca95fd019b34d1
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-05-11 21:19:38 -0500

    Fixed eslint issue

--- config/middleware/isAuthen.js
+++ config/middleware/isAuthen.js
@@ -1,10 +1,9 @@
-module.exports = function(req, res, next) {
-    // If logged in, continue 
-    if (req.user) {
-      return next();
-    }
-  
-    // else redirect to home
-    return res.redirect("/");
-  };
-  
+module.exports = function (req, res, next) {
+  // If logged in, continue 
+  if (req.user) {
+    return next();
+  }
+
+  // else redirect to home
+  return res.redirect("/");
+};
