3be4578905b3ae08ea6be54d834db264213a6a68
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-05-14 21:31:15 -0500

    Connected routes with respective files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,21 +27,25 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+     res.sendFile(path.join(__dirname, "../public/login.html"));
+
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    //res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render('../views/index.handlebars')
   });
 
   app.get("/add", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
+    // res.sendFile(path.join(__dirname, "../public/add.html"));
+    res.render('../views/additems.handlebars')
   });
 
   app.get("/explore", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/explore.html"));
+    // res.sendFile(path.join(__dirname, "../public/explore.html"));
+    res.render('../views/recipes.handlebars')
   });
 
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // Requiring npm packages
 var express = require("express");
 var session = require("express-session");
+const exphbs = require("express-handlebars")
 
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
@@ -19,6 +20,13 @@ app.use(express.static("public"));
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+/////////////
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars");
+
+/////////////
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
