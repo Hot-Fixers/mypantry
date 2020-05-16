689aa8b1736957fee20e3e03ff41dfbb7ee888ed
julzar <56980945+julzar@users.noreply.github.com>
2020-05-16 11:22:18 -0500

    updated add/explore routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,8 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models");
+const axios = require("axios");
+require("dotenv").config();
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthen");
@@ -38,14 +41,51 @@ module.exports = function (app) {
     res.render('../views/index.handlebars');
   });
 
-  app.get("/add", isAuthenticated, function (req, res) {
-    // res.sendFile(path.join(__dirname, "../public/add.html"));
-    res.render('../views/additems.handlebars');
+  app.get("/add", isAuthenticated, async function (req, res) {
+    try {
+      let query = {};
+      if (req.query.User_id) {
+        query.UserId = req.query.User_id;
+      };
+      let ingredients = await db.Ingredients.findAll({
+        where: query
+      });
+      res.render("pantry", {items: ingredients.map(i => i.dataValues)});
+    }
+    catch (err) {
+      throw err;
+    };
   });
 
-  app.get("/explore", isAuthenticated, function (req, res) {
-    // res.sendFile(path.join(__dirname, "../public/explore.html"));
-    res.render('../views/recipes.handlebars');
+  app.get("/explore", isAuthenticated, async function (req, res) {
+    try {
+      const key = process.env.KEY
+      let query = {};
+      if (req.query.User_id) {
+        query.UserId = req.query.User_id;
+      };
+      let ingredients = await db.Ingredients.findAll({
+        where: query
+      });
+      let iMapped = ingredients.map(i => i = i.Ingredients).join(",+");
+      let url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${iMapped}&number=2&ranking=2&apiKey=${key}`;
+      let dishes = await axios.get(url);
+      let dMapped = dishes.data.map(d => d = d.id);
+      let steps = [];
+      for (let d of dMapped) {
+        let s = await axios.get(`https://api.spoonacular.com/recipes/${d}/analyzedInstructions?apiKey=${key}`);
+        steps.push({steps: s.data[0].steps});
+      }
+      let combined =[];
+      for (let i = 0; i < dMapped.length; i++) {
+        let c = {...dishes.data[i], ...steps[i]};
+        combined.push(c);
+      }
+      res.render("recipes", {recipes: combined});
+    }
+    catch (err) {
+      throw err;
+    };
   });
 
 
