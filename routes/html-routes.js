954422355458689f699f7b36336cc483c1b34d98
julzar <56980945+julzar@users.noreply.github.com>
2020-05-18 17:52:58 -0500

    update explore route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -73,8 +73,8 @@ module.exports = function (app) {
       const steps = [];
       for (let d of dMapped) {
         let s = await axios.get(`https://api.spoonacular.com/recipes/${d}/analyzedInstructions?apiKey=${key}`);
-        if (s.data === []) {
-          delete s;
+        if (s.data.length === 0) {
+          steps.push({steps: ["Sorry, no instructions available."]})
         } else {
           steps.push({
             steps: s.data[0].steps
